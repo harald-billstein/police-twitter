93d453108a36d9f97dc11cf60bfac61d835bc0b6
Harald Billstein <harald.billstein@gmail.com>
2023-03-13 22:47:42 +0100

    using summary and not extended info

--- src/main/java/se/harbil/policetwitter/util/MessageComposer.java
+++ src/main/java/se/harbil/policetwitter/util/MessageComposer.java
@@ -6,24 +6,25 @@ import se.harbil.policetwitter.model.PoliceEventKafkaModel;
 @Service
 public class MessageComposer {
 
-    private String createMessage(PoliceEventKafkaModel event, String hashTag, int messageSize) {
-        int maxMessageSize = 280;
+    private final int maxMessageSize = 280;
+
+    private String createMessage(String message, String hashTag, int messageSize) {
         if (messageSize > maxMessageSize) {
-            int i = 3 + event.getSummary().length() + hashTag.length() - maxMessageSize;
-            return event.getSummary().substring(0, (event.getSummary().length() - i)) + "..." + hashTag;
+            int i = 3 + message.length() + hashTag.length() - maxMessageSize;
+            return message.substring(0, (message.length() - i)) + "..." + hashTag;
         } else {
-            return event.getSummary() + hashTag;
+            return message + hashTag;
         }
     }
 
     public String createMessageNotLongerThen280Char(PoliceEventKafkaModel event, String hashTag) {
         int messageSize;
-        //if (null != event.getExtendedInfo()) {
-        //    messageSize = event.getExtendedInfo().length() + hashTag.length();
-        //    return createMessage(event.getExtendedInfo(), hashTag, messageSize);
-        //} else {
-            messageSize = event.getDatetime().length() + event.getSummary().length() + hashTag.length();
-            return createMessage(event, hashTag, messageSize);
-        //}
+       // if (null != event.getExtendedInfo()) {
+       //     messageSize = event.getSummary().length() + hashTag.length();
+       //     return createMessage(event.getExtendedInfo(), hashTag, messageSize);
+       // } else {
+            messageSize = event.getSummary().length() + hashTag.length();
+            return createMessage(event.getSummary(), hashTag, messageSize);
+        }
     }
 }
