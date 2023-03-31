9014e514d3141f31b2667e54e62f7bb5fe712dc1
fhbi26 <harald.billstein@gmail.com>
2023-03-31 14:19:05 +0200

    Link at end of tweet fix

--- src/main/java/se/harbil/policetwitter/util/MessageComposer.java
+++ src/main/java/se/harbil/policetwitter/util/MessageComposer.java
@@ -17,17 +17,16 @@ public class MessageComposer {
     private String createMessage(PoliceEventKafkaModel event, String hashTag, int messageSize) {
         if (messageSize > maxMessageSize) {
             int i = 3 + event.getSummary().length() + hashTag.length() + event.getUrl().length() - maxMessageSize;
-            return event.getSummary().substring(0, (event.getSummary().length() - i)) + "..." + event.getUrl() + hashTag;
+            return event.getSummary().substring(0, (event.getSummary().length() - i)) + "..." + hashTag + event.getUrl();
         } else {
-            return event.getSummary() + " " + event.getUrl() + " " + hashTag;
+            return event.getSummary()  + hashTag + event.getUrl();
         }
     }
 
     public String createMessageNotLongerThen280Char(PoliceEventKafkaModel event) {
         String hashTag = hashTagCreator.getHashTag(event);
         int urlSize = 23;
-        int spaceInMessage = 2;
-        int messageSize = event.getSummary().length() + hashTag.length() + urlSize+ spaceInMessage;
+        int messageSize = event.getSummary().length() + hashTag.length() + urlSize;
         return createMessage(event, hashTag, messageSize);
     }
 }
--- src/test/java/se/harbil/policetwitter/util/MessageComposerTest.java
+++ src/test/java/se/harbil/policetwitter/util/MessageComposerTest.java
@@ -1,65 +1,84 @@
 package se.harbil.policetwitter.util;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.harbil.policetwitter.model.PoliceEventKafkaModel;
 import se.harbil.policetwitter.service.HashTagCreator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MessageComposerTest {
 
     MessageComposer messageComposer;
     HashTagCreator hashTagCreator;
+
     @BeforeEach
     void setUp() {
         hashTagCreator = new HashTagCreator();
         messageComposer = new MessageComposer(hashTagCreator);
     }
+
     @Test
-    void testMessageMaxSizeShouldNotCutMessage() {
-        PoliceEventKafkaModel event = PoliceEventKafkaModel.builder()
+    void testMessageMaxSizeShouldNotCutMessageIsExact280Char() {
+        PoliceEventKafkaModel event = eventContaining280Char();
+
+        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
+        System.out.println(messageNotLongerThen280Char);
+
+        assertEquals(280, messageNotLongerThen280Char.length());
+        assertFalse(messageNotLongerThen280Char.contains("..."));
+        assertEquals("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan! #Stockholm #fjällräddning https://fixLenght23.com", messageNotLongerThen280Char);
+    }
+
+    @Test
+    void testMessageToLongShouldCutMessage() {
+        PoliceEventKafkaModel event = eventContaining281Char();
+
+        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
+
+        assertEquals(280, messageNotLongerThen280Char.length());
+        assertTrue(messageNotLongerThen280Char.contains("..."));
+        assertEquals("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen möss... #Stockholm #fjällräddning https://fixLenght23.com", messageNotLongerThen280Char);
+    }
+
+    @Test
+    void testMessageLessThen280Chars() {
+        PoliceEventKafkaModel event = eventContaining279Char();
+
+        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
+
+        assertTrue(messageNotLongerThen280Char.length() < 280);
+        assertFalse(messageNotLongerThen280Char.contains("..."));
+        assertEquals("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan #Stockholm #fjällräddning https://fixLenght23.com", messageNotLongerThen280Char);
+    }
+
+    private static PoliceEventKafkaModel eventContaining280Char() {
+        return PoliceEventKafkaModel.builder()
+                .locationName("Stockholm")
+                .summary("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan!")
+                .url("https://fixLenght23.com")
+                .type("fjällräddning")
+                .build();
+    }
+
+    private static PoliceEventKafkaModel eventContaining281Char() {
+        return PoliceEventKafkaModel.builder()
+                .locationName("Stockholm")
+                .summary("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan!!")
+                .url("https://fixLenght23.com")
+                .type("fjällräddning")
+                .build();
+    }
+
+    private static PoliceEventKafkaModel eventContaining279Char() {
+        return PoliceEventKafkaModel.builder()
                 .locationName("Stockholm")
                 .summary("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan")
                 .url("https://fixLenght23.com")
                 .type("fjällräddning")
                 .build();
-
-        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
-
-        Assertions.assertTrue(messageNotLongerThen280Char.length() == 280);
-        Assertions.assertFalse(messageNotLongerThen280Char.contains("..."));
-    }
-
-    @Test
-    void testMessageToLongShouldCutMessage() {
-        PoliceEventKafkaModel event = PoliceEventKafkaModel.builder()
-                .locationName("Stockholm")
-                .summary("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning då en elev i en skolklass med en elev som glömt sin mössa hemma, fröken var mycket tacksam och tyckte om färgen mössan!!")
-                .url("https://fixLenght23.com")
-                .type("fjällräddning")
-                .build();
-
-        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
-
-        Assertions.assertTrue(messageNotLongerThen280Char.length() == 280);
-        Assertions.assertTrue(messageNotLongerThen280Char.contains("..."));
-    }
-
-    @Test
-    void testMessageLessThen280Chars() {
-        PoliceEventKafkaModel event = PoliceEventKafkaModel.builder()
-                .locationName("Stockholm")
-                .summary("Hund letade i snön en längre stund och hittade en mössa. Tyvärr hittades ej människorna, men mössan kom till användning för en skolklass med en elev som glömt sin mössa hemma, fröken var tacksam och tyckte om färgen mössan")
-                .url("https://fixLenght23.com")
-                .type("fjällräddning")
-                .build();
-
-        String messageNotLongerThen280Char = messageComposer.createMessageNotLongerThen280Char(event);
-
-        System.out.println(messageNotLongerThen280Char.length());
-        Assertions.assertTrue(messageNotLongerThen280Char.length() < 280);
-
-        Assertions.assertFalse(messageNotLongerThen280Char.contains("..."));
     }
 
 }
